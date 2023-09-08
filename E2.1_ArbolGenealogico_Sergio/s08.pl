25001d29dbdcd3c8f12fcf22cdbb00f7961655e4
Sergio_Montaño <71527413+sergio-jose-aguas-montano@users.noreply.github.com>
2023-09-08 01:39:01 -0600

    E2.1_Tios y primos
    Agregación de consultas para buscar parentesco con tios, tías y primos (quedan aun pendientes por error de ciclacion).

--- E2.1_ArbolGenealogico_Sergio/s08.pl
+++ E2.1_ArbolGenealogico_Sergio/s08.pl
@@ -5,34 +5,41 @@ hombre('Isidro').
 hombre('Abelino').
 hombre('Guillermo').
 hombre('Gabriel').
-hombre('Andrés').
+hombre('Andres').
+hombre('Oscar').
 
 mujer('Matilde').
 mujer('Melanie').
 mujer('Karem').
 mujer('Jovita').
 mujer('Teresa').
+mujer('Tere').
+mujer('Juana').
 
 % Progenitores
 progenitor('Isidro','Matilde').
 progenitor('Isidro','Guillermo').
+progenitor('Isidro','Tere').
 progenitor('Abelino','Gabriel').
 progenitor('Abelino','Sergio').
-progenitor('Abelino','Andrés').
+progenitor('Abelino','Andres').
 progenitor('Sergio','Sergiojr').
 progenitor('Sergio','Cristian').
 progenitor('Sergio','Karem').
 progenitor('Sergio','Melanie').
+progenitor('Gabriel','Oscar').
 
 progenitor('Jovita','Sergio').
 progenitor('Jovita','Gabriel').
-progenitor('Jovita','Andrés').
+progenitor('Jovita','Andres').
 progenitor('Teresa','Guillermo').
 progenitor('Teresa','Matilde').
+progenitor('Teresa','Tere').
 progenitor('Matilde','Sergiojr').
 progenitor('Matilde','Cristian').
 progenitor('Matilde','Karem').
 progenitor('Matilde','Melanie').
+progenitor('Juana','Oscar').
 
 % Relacion/Parentesco
 padre(P,H) :- progenitor(P,H), hombre(P).
@@ -51,7 +58,23 @@ abuela_mat(A,N) :- madre(A,M), madre(M,N).
 abuelos_pat(Ao,Aa,N) :- abuelo_pat(Ao,N), abuela_pat(Aa,N).
 abuelos_mat(Ao,Aa,N) :- abuelo_mat(Ao,N), abuela_mat(Aa,N).
 
-tio_mat(T,S) :- madre(M,S), findall(Tio, (hermano(Tio,M), hombre(Tio)), Tios), list_to_set(Tios,T).
-tio_pat(T,S) :- padre(P,S), findall(Tio, (hermano(Tio,P), hombre(Tio)), Tios), list_to_set(Tios,T).
+hermanos(H1,H2) :- padre(P,H1), padre(P,H2), H1\=H2.
 
-hermano(H,S) :- progenitor(P,H), progenitor(P,S), H \= S, hombre(H).
+%tio(T,S) :- hermanos(T,X), padre(X,S), hombre(T), T\=X.
+%tia(T,S) :- hermanos(T,X), madre(X,S), mujer(T), T\=X.
+
+tio(T, S) :- (hermanos(T, X), padre(X, S), hombre(T), T\=X) ; (hermanos(T, Y), madre(Y, S), hombre(T), T\=Y).
+tia(T, S) :- (hermanos(T, X), madre(X, S), mujer(T), T\=X) ; (hermanos(T, Y), padre(Y, S), mujer(T), T\=Y).
+
+
+% primos(P1, P2) :- abuelos_mat(AM1, AM2, P1), abuelos_pat(AP1, AP2, P2),
+% AM1 \= AP1, AM2 \= AP2.
+
+% primos(P1, P2) :- abuelo_mat(AM, P1), abuelo_pat(AP, P2),
+% abuelo_mat(AM, P2), abuelo_pat(AP, P1), P1 \= P2, \+ hermanos(P1, P2).
+
+primos(P1, P2) :- P1\=P2, abuelos_mat(AM1, AM2, P1),abuelos_pat(AP1, AP2, P2),AM1 \= AP1,AM2 \= AP2,\+ hermanos(P1, P2).
+
+primos(P1, P2) :- P1\=P2, abuelos_mat(AM1, AM2, P2),abuelos_pat(AP1, AP2, P1),AM1 \= AP1,AM2 \= AP2,\+ hermanos(P1, P2).
+
+primos(Persona, Primos) :- findall(Primo, (primos(Persona, Primo), Persona \= Primo), Primos).
