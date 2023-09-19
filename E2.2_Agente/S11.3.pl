e183d27bea11e9aa085518e7a9482a496d8c44da
Sergio_Montaño <71527413+sergio-jose-aguas-montano@users.noreply.github.com>
2023-09-19 11:32:54 -0600

    Resubida de archivos
    Error en ficheros subidos xd.

---
+++ E2.2_Agente/S11.3.pl
@@ -0,0 +1,8 @@
+:- dynamic mamifero/1.
+
+mamifero(delfin).
+mamifero(leon).
+mamifero(halcon).
+mamifero(raton).
+mamifero(lagarto).
+
---
+++ E2.2_Agente/s11.2.pl
@@ -0,0 +1,15 @@
+%Consultar el archivo S11
+inicio :- consult('SSPIAI_Mercado.Pedroza.Saul/S11.3.pl').
+
+agregar(A) :- assert(mamifero(A)).
+buscar(A) :- respuesta(A).
+eliminar(A) :- retract(mamifero(A)).
+%Guardar con tell, listing, told
+guardar :- tell('SSPIAI_Mercado.Pedroza.Saul/S11.3.pl'),
+        listing(mamifero/1),told,
+        write("Guardado ...").
+
+fin :- retractall(mamifero(_)).
+
+respuesta(A) :- mamifero(A), write('es un animal').
+respuesta(A) :- \+mamifero(A), agregar(A), write("Nuevo Conocimiento").
