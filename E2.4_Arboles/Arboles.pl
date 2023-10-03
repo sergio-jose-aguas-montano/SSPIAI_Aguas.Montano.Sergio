24333cb3ca6b715221039c86c7eb459921ceab02
Sergio_Montaño <71527413+sergio-jose-aguas-montano@users.noreply.github.com>
2023-10-03 09:48:40 -0600

    [2.4] - Actualizacion del trabajo
    Se corrigió la imagen y se agrego elemento que faltaba, en el .pl, se añadio una prueba de lo que será el nombre completo en forma de árbol

--- E2.4_Arboles/Arboles.pl
+++ E2.4_Arboles/Arboles.pl
@@ -3,16 +3,16 @@
 %arbol(raiz,hizq,hder).
 
 arbol(a,arbol(e,i,o),u).
+arbol(arbol(arbol(arbol(s,e,r),g,arbol(i,o,j)),o,arbol(arbol(s,e,a),g,arbol(u,a,s))),m,arbol(arbol(o,n,t),a,arbol(n,o,' '))).
 
 %Raiz, Izq, Der
 preorden(arbol(Raiz, HIzq, HDer)) :-  write(Raiz), preorden(HIzq), preorden(HDer), !.
 preorden(H) :- write(H).
 
 %Izq, Raiz, Der
-inorden(arbol(Raiz, Hizq, HDer)) :- inorden(Hizq), write(Raiz), inorden(HDer).
+inorden(arbol(Raiz, Hizq, HDer)) :- inorden(Hizq), write(Raiz), inorden(HDer), !.
 inorden(H) :- write(H).
 
 %Izq, Der, Raiz
 postorden(arbol(Raiz, HIzq, HDer)) :- postorden(HIzq), postorden(HDer), write(Raiz), !.
 postorden(H) :- write(H).
-
