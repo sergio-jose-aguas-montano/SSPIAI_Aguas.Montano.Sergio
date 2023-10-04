a51b5a8ed0de165c1112be094249c7df953ae50f
Sergio_Montaño <71527413+sergio-jose-aguas-montano@users.noreply.github.com>
2023-10-03 21:45:42 -0600

    [E2.4] - Actializacion final
    Se arreglaron unos problemas con las imagenes donde se confundieron el orden de unos nodos, además corrección de lógica en el código, agregado de los trabajos (Nombre y apellido, Nombre completo, Apellido con mas caracteres).

--- E2.4_Arboles/Arboles.pl
+++ E2.4_Arboles/Arboles.pl
@@ -2,17 +2,28 @@
 
 %arbol(raiz,hizq,hder).
 
-arbol(a,arbol(e,i,o),u).
-arbol(arbol(arbol(arbol(s,e,r),g,arbol(i,o,j)),o,arbol(arbol(s,e,a),g,arbol(u,a,s))),m,arbol(arbol(o,n,t),a,arbol(n,o,' '))).
+%arbol(a,arbol(e,i,o),u).
+
+%Prueba a,e,i,o,u
+%arbol(a,arbol(e,i,o),u)
+
+%Nombre Completo Inorden
+arbol(arbol(arbol(arbol('S','e','r'),'g',arbol('i','o ','J')),'o',arbol(arbol('s','e ','A'),'g',arbol('u','a','s '))),'M',arbol(arbol('o','n','t'),'a',arbol('n','o',' '))).
+
+%Apellido con mas caracteres Preorden.
+arbol('M',arbol('o','n','t'),arbol('a','n','o')).
+
+%Nombre y apellido Postorden
+arbol(arbol(arbol('S','e','r'),arbol('g','i','o '),'A'),arbol('g','u','a'),'s').
 
 %Raiz, Izq, Der
 preorden(arbol(Raiz, HIzq, HDer)) :-  write(Raiz), preorden(HIzq), preorden(HDer), !.
 preorden(H) :- write(H).
 
 %Izq, Raiz, Der
-inorden(arbol(Raiz, Hizq, HDer)) :- inorden(Hizq), write(Raiz), inorden(HDer), !.
+inorden(arbol(HIzq, Raiz, HDer)) :- inorden(HIzq), write(Raiz), inorden(HDer), !.
 inorden(H) :- write(H).
 
 %Izq, Der, Raiz
-postorden(arbol(Raiz, HIzq, HDer)) :- postorden(HIzq), postorden(HDer), write(Raiz), !.
+postorden(arbol(HIzq, HDer, Raiz)) :- postorden(HIzq), postorden(HDer), write(Raiz), !.
 postorden(H) :- write(H).
